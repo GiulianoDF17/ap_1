596893a3f47cc5cea656027e248ebedef76ee1f3
Giuliano Di Fulvio <2578593@edu.vaniercollege.qc.ca>
2025-10-26 21:26:52 -0400

    Updated the loop + questions

--- Automaton.java
+++ Automaton.java
@@ -48,16 +48,14 @@ public class Automaton
     {
         // Build the new state in a separate array.
         int[] nextState = new int[state.length];
-        // Naively update the state of each cell
-        // based on the state of its two neighbors.
-        for(int i = 0; i < state.length; i++) {
-            int left = (i == 0) ? 0 : state[i - 1];
-            int center = state[i];
+        int left = 0;
+        int center = state[0];
+        for (int i = 0; i < state.length; i++) {
             int right = (i + 1 < state.length) ? state[i + 1] : 0;
-            
             nextState[i] = (left + center + right) % 2;
+            left = center;
+            center = right;
         }
-        state = nextState;
     }
     
     /**
