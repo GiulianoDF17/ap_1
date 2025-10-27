0328ca41b96bf21e5f42655de01a97884031b63f
Giuliano Di Fulvio <2578593@edu.vaniercollege.qc.ca>
2025-10-26 21:27:45 -0400

    added calculateNextState

--- Automaton.java
+++ Automaton.java
@@ -58,6 +58,10 @@ public class Automaton
         }
     }
     
+    private int calculateNextState(int left, int center, int right){
+        return (left + center + right) % 2;
+    }
+    
     /**
      * Reset the automaton.
      */
